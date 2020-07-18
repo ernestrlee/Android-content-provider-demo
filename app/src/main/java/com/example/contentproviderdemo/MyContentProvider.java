842dc3603a7c6dd5b8fe846b4958630b7cdbf1da
Ernest-Lee <ernestrlee@yahoo.com>
2020-07-18 14:35:50 -0700

    Added SQLite class. Modified URI name.

--- app/src/main/java/com/example/contentproviderdemo/MyContentProvider.java
+++ app/src/main/java/com/example/contentproviderdemo/MyContentProvider.java
@@ -2,7 +2,10 @@ package com.example.contentproviderdemo;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 
 public class MyContentProvider extends ContentProvider {
@@ -10,7 +13,28 @@ public class MyContentProvider extends ContentProvider {
     }
 
     public static final String AUTHORITY = "com.example.content.provider";
-    public static final Uri CONTENT_URI_1 = Uri.parse("content://" + AUTHORITY);
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    private class MyDatabase extends SQLiteOpenHelper {
+        private static final String DB_NAME = "company";
+        private static final String DB_TABLE = "emp";
+        private static final int DB_VER = 1;
+
+        public MyDatabase(Context ct){
+            super(ct, DB_NAME, null, DB_VER);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("create table " + DB_TABLE + " (_id integer primary key autoincrement, emp_name text, profile text);");
+
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("drop table if exists " + DB_TABLE);
+        }
+    }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
